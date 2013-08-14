cae6163c367a97ebad3c8790300993381177ecd2
Eric89GXL <larson.eric.d@gmail.com>
2013-08-13 17:30:39 -0700

    Update __init__.py

--- expyfun/__init__.py
+++ expyfun/__init__.py
@@ -4,7 +4,7 @@
 __version__ = '0.1.git'
 
 # have to import verbose first since it's needed by many things
-from .utils import set_log_level, verbose, set_config, \
+from .utils import set_log_level, verbose_dec, set_config, \
                    get_config, get_config_path
 
 from .experiment_controller import ExperimentController
