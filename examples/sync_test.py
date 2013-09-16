fab3fe32816b16608852f7cdf997d8911413c426
Eric Larson <larson.eric.d@gmail.com>
2013-09-16 11:36:15 -0700

    ENH: Clearer params

--- examples/sync_test.py
+++ examples/sync_test.py
@@ -21,17 +21,15 @@ with ExperimentController('SyncTest', screen_num=0, window_size=[300, 300],
                           stim_fs=24414, participant='s', session='0',
                           output_dir=None) as ec:
     ec.load_buffer(np.r_[0.1, np.zeros(2000)])
+    black = [1, 1, 1]
+    white = [-1, -1, -1]
     while True:
-        ec.window.setColor([1, 1, 1])
-        #ec.flip_and_play()
+        ec.window.setColor(white)
+        ec.flip_and_play()
         #ec.flip()                  # expyfun
-        #ec.window.flip()           # psychopy
-        ec.window.winHandle.flip()  # pyglet
-        ec._ac.play()
-        ec.wait_one_press(rng.rand(1) * 0.0167)
+        #ec._ac.play()
+        ec.wait_one_press(0.5 + rng.rand(1) * 0.0167)
         ec.stop()
-        ec.window.setColor([-1, -1, -1])
-        #ec.flip()                  # expyfun
-        #ec.window.flip()           # psychopy
-        ec.window.winHandle.flip()  # pyglet
-        ec.wait_one_press(rng.rand(1) * 0.0167)
+        ec.window.setColor(black)
+        ec.flip()                  # expyfun
+        ec.wait_one_press(rng.rand(1) * 5 * 0.0167)
