899d512b7ebb4a307faa188cd44a0cdc4a057a0d
Eric89GXL <larson.eric.d@gmail.com>
2013-11-14 12:49:01 -0800

    FIX: Add EXPYFUN_EYELINK to known_config_types

--- expyfun/_utils.py
+++ expyfun/_utils.py
@@ -280,6 +280,7 @@ def get_config_path():
 # List the known configuration values
 known_config_types = ['RESPONSE_DEVICE',
                       'AUDIO_CONTROLLER',
+                      'EXPYFUN_EYELINK',
                       'TDT_MODEL',
                       'TDT_INTERFACE',
                       'TDT_CIRCUIT_PATH',
