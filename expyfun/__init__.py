66a358a84cbb2a33dce97f77afdb7b528e9d4dcd
drammock <dan.mccloy@gmail.com>
2014-01-07 11:33:08 -0800

    minor tweak to make logging defaults less annoying

--- expyfun/__init__.py
+++ expyfun/__init__.py
@@ -4,7 +4,7 @@
 __version__ = '1.1.0.git'
 
 # have to import verbose first since it's needed by many things
-from ._utils import (set_log_level, set_config,
+from ._utils import (set_log_level, set_log_file, set_config,
                      get_config, get_config_path)
 from ._utils import verbose_dec as verbose
 from ._experiment_controller import ExperimentController, wait_secs
@@ -13,5 +13,6 @@ from ._create_system_config import create_system_config
 from . import analyze  # fast enough, include here
 from . import stimuli
 
-# initialize logging
+# INIT LOGGING
 set_log_level(None, False)
+set_log_file()
