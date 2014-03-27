a79a05f8fa6655f0ff0218d82a64c522e5acb190
Eric Larson <larson.eric.d@gmail.com>
2014-03-27 16:22:46 -0700

    FIX: Quieter example

--- examples/advanced_stimuli.py
+++ examples/advanced_stimuli.py
@@ -19,11 +19,11 @@ dur = 0.5
 freq = 500.
 # let's make a square wave
 sig = np.sin(freq * 2 * np.pi * np.arange(dur * fs, dtype=float) / fs)
-sig = (sig > 0 - 0.5) * 2.
+sig = (sig > 0 - 0.5) / 10.  # make it reasonably quiet for play_sound
 sig = window_edges(sig, fs)
 
-play_sound(sig, wait=True)
+play_sound(sig, norm=False, wait=True)
 
 move_sig = np.concatenate([convolve_hrtf(sig, fs, ang)
                            for ang in range(-90, 91, 15)], axis=1)
-play_sound(move_sig, wait=True)
+play_sound(move_sig, norm=False, wait=True)
