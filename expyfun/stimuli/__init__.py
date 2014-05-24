bfb5807768d862e65015c7545b4074d8b8905381
drammock <dan.mccloy@gmail.com>
2014-05-23 17:48:32 -0700

    almost working...

--- expyfun/stimuli/__init__.py
+++ expyfun/stimuli/__init__.py
@@ -6,4 +6,4 @@ from ._stimuli import rms, read_wav, write_wav, play_sound, window_edges
 from ._hrtf import convolve_hrtf
 from ._mls import (compute_mls_impulse_response, repeated_mls,
                    _max_len_seq)
-from ._vocoder import vocode_ci
+from ._vocoder import vocode, band_envs
--- expyfun/stimuli/_vocoder.py
+++ expyfun/stimuli/_vocoder.py
@@ -19,9 +19,10 @@ def _erbn_to_freq(e):
 
 
 @verbose_dec
-def vocode_ci(data, fs, n_bands=16, freq_lims=(200., 8000.), order=2,
-              env_lp=160., env_lp_order=4, mode='noise', rand_seed=None,
-              axis=-1, verbose=None):
+def vocode(data, fs, n_bands=16, freq_lims=(200., 8000.), scale='erb',
+           order=2, env_lp=160., env_lp_order=4, mode='noise',
+           poisson_rate=200, poisson_linked=False,
+           rand_seed=None, axis=-1, verbose=None):
     """Vocode stimuli using a CI simulation
 
     Adapted from an algorithm described by Zachary Smith (Cochlear).
@@ -36,6 +37,9 @@ def vocode_ci(data, fs, n_bands=16, freq_lims=(200., 8000.), order=2,
         Number of bands to use.
     freq_lims : tuple
         2-element list of lower and upper frequency bounds.
+    scale : str
+        Scale on which to equally space the bands. Possible values are "erb",
+        "loghz" (base-2), and "hz".
     order : int
         Order of analysis and synthesis.
         NOTE: Using too high an order can cause instability,
@@ -45,7 +49,15 @@ def vocode_ci(data, fs, n_bands=16, freq_lims=(200., 8000.), order=2,
     evn_lp_order : int
         Order of the envelope low-pass.
     mode : str
-        Use 'noise' or 'tone' for noise- or tone-vocoding.
+        The type of signal used to excite each band. Options are "noise" for
+        band-limited noise, "tone" for sinewave-at-center-frequency, or
+        "poisson" for a poisson process of band-limited clicks at the rate
+        given by ``poisson_rate``.
+    poisson_rate : int
+        Average number of clicks per second for the poisson train used to
+        excite each band (when mode=="poisson").
+    poisson_linked : bool
+        Whether the same poisson process is used to excite all bands.
     rand_seed : int | None
         Random seed to use. If None, no seeding is done.
     axis : int
@@ -60,30 +72,111 @@ def vocode_ci(data, fs, n_bands=16, freq_lims=(200., 8000.), order=2,
     -----
     This will use ERB-spaced frequency windows.
     """
-    data = np.array(data, float)  # will make a copy
-    n_samp = data.shape[axis]
-    voc = np.zeros_like(data)
-    freq_lims = np.array(freq_lims, float)
-    fs = float(fs)
-    if np.any(freq_lims >= fs / 2.) or env_lp >= fs / 2.:
-        raise ValueError('frequency limits must not exceed Nyquist')
-    assert freq_lims.ndim == 1 and freq_lims.size == 2
-    if mode not in ('noise', 'tone'):
-        raise ValueError('mode must be "noise" or "tone", not {0}'
+    # check args
+    if mode not in ('noise', 'tone', 'poisson'):
+        raise ValueError('mode must be "noise", "tone", or "poisson", not {0}'
                          ''.format(mode))
     if rand_seed is None:
         rng = np.random
     else:
         rng = np.random.RandomState(rand_seed)
-    freq_lims_erbn = _freq_to_erbn(freq_lims)
-    delta_erb = np.diff(freq_lims_erbn) / n_bands
-    cutoffs = _erbn_to_freq(freq_lims_erbn[0] +
-                            delta_erb * np.arange(n_bands + 1))
-    assert np.allclose(cutoffs[[0, -1]], freq_lims)  # should be
+    # get band envelopes
+    bands = band_envs(data, fs, n_bands=n_bands, freq_lims=freq_lims,
+                      scale=scale, order=order, env_lp=env_lp,
+                      env_lp_order=env_lp_order, axis=axis, verbose=verbose)
+
+    n_samp = data.shape[axis]
+    voc = np.zeros_like(data)
+
+    # reconstruct
+    if poisson_linked and mode == 'poisson':  # same for each band
+        carrier = rng.random_integers(0, 1, n_samp).astype(float)
+    for cf, env, (b, a) in bands:
+        if mode == 'tone':
+            carrier = np.sin(2 * np.pi * cf * np.arange(n_samp) / fs)
+            carrier *= np.sqrt(2)  # rms of 1
+            shape = np.ones_like(data.shape)
+            shape[axis] = n_samp
+            carrier.shape = shape
+        else:
+            if mode == 'noise':
+                carrier = rng.rand(*data.shape)
+            else:  # mode == 'poisson'
+                if not poisson_linked:  # different for each band
+                    carrier = rng.random_integers(0, 1, n_samp).astype(float)
+            carrier = lfilter(b, a, carrier, axis=axis)
+            carrier /= np.sqrt(np.mean(carrier * carrier, axis=axis,
+                                       keepdims=True))  # rms of 1
+        voc += carrier * env
+    return voc
+
+
+@verbose_dec
+def band_envs(data, fs, n_bands=16, freq_lims=(200., 8000.), scale='erb',
+              order=2, env_lp=160., env_lp_order=4, axis=-1, verbose=None):
+    """Calculate frequency band envelopes of a signal
+
+    Parameters
+    ----------
+    data : array-like
+        Data array.
+    fs : float
+        Sample rate.
+    n_bands : int
+        Number of bands to use.
+    freq_lims : tuple
+        2-element list of lower and upper frequency bounds (in Hz).
+    scale : str
+        Scale on which to equally space the bands. Possible values are "erb",
+        "loghz" (base-2), and "hz".
+    order : int
+        Order of analysis and synthesis.
+        NOTE: Using too high an order can cause instability,
+        always check outputs for order > 2!
+    env_lp : float
+        Frequency of the envelope low-pass.
+    evn_lp_order : int
+        Order of the envelope low-pass.
+    axis : int
+        Axis to operate over.
+
+    Returns
+    -------
+    edges, cfs, env : list of tuples
+        List of tuples: (center frequency, envelope, (filter numer, denom)).
+
+    Notes
+    -----
+    Adapted from an algorithm described by Zachary Smith (Cochlear Corp.).
+    """
+
+    data = np.array(data, float)  # will make a copy
+    freq_lims = np.array(freq_lims, float)
+    fs = float(fs)
+    if np.any(freq_lims >= fs / 2.) or env_lp >= fs / 2.:
+        raise ValueError('frequency limits must not exceed Nyquist')
+    assert freq_lims.ndim == 1 and freq_lims.size == 2
+    if scale not in ('erb', 'loghz', 'hz'):
+        raise ValueError('Frequency scale must be "erb", "hz", or "loghz".')
+    if scale == 'erb':
+        freq_lims_erbn = _freq_to_erbn(freq_lims)
+        delta_erb = np.diff(freq_lims_erbn) / n_bands
+        cutoffs = _erbn_to_freq(freq_lims_erbn[0] +
+                                delta_erb * np.arange(n_bands + 1))
+        assert np.allclose(cutoffs[[0, -1]], freq_lims)  # should be
+    elif scale == 'loghz':
+        freq_lims_log = np.log2(freq_lims)
+        delta = np.diff(freq_lims_log) / n_bands
+        cutoffs = 2. ** (freq_lims_log[0] + delta * np.arange(n_bands + 1))
+        assert np.allclose(cutoffs[[0, -1]], freq_lims)  # should be
+    else:  # scale == 'hz'
+        delta = np.diff(freq_lims) / n_bands
+        cutoffs = freq_lims[0] + delta * np.arange(n_bands + 1)
     cfs = list(np.round((cutoffs[:-1] + cutoffs[1:]) / 2.).astype(int))
     logger.info('Using frequencies centered at: {0}'.format(cfs))
-
     # extract the envelope in each band
+    envs = []
+    filts = []
     for lf, hf in zip(cutoffs[:-1], cutoffs[1:]):
         # band-pass
         b, a = butter(order, [lf / fs, hf / fs], 'bandpass')
@@ -92,18 +185,6 @@ def vocode_ci(data, fs, n_bands=16, freq_lims=(200., 8000.), order=2,
         env[env < 0] = 0.0
         b_env, a_env = butter(env_lp_order, env_lp / fs, 'lowpass')
         env = lfilter(b_env, a_env, env, axis=axis)
-        # reconstruct
-        if mode == 'tone':
-            cf = (lf + hf) / 2.
-            carrier = np.sin(2 * np.pi * cf * np.arange(n_samp) / fs)
-            carrier *= np.sqrt(2)  # rms of 1
-            shape = np.ones_like(data.shape)
-            shape[axis] = n_samp
-            carrier.shape = shape
-        else:  # mode == 'noise'
-            carrier = rng.rand(*data.shape)
-            carrier = lfilter(b, a, carrier, axis=axis)
-            carrier /= np.sqrt(np.mean(carrier * carrier, axis=axis,
-                                       keepdims=True))  # rms of 1
-        voc += carrier * env
-    return voc
+        envs.append(env)
+        filts.append((b, a))
+    return(zip(cfs, envs, filts))
