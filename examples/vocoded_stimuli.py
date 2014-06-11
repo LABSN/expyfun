f68599e649b6f1b53a007af733456a5b5c2f7023
Daniel McCloy <dan.mccloy@gmail.com>
2014-06-10 19:50:37 -0700

    simplified example to use built-in poisson generation

--- examples/vocoded_stimuli.py
+++ examples/vocoded_stimuli.py
@@ -11,8 +11,7 @@ This shows how to make simple vocoded stimuli.
 
 import numpy as np
 
-from expyfun.stimuli import (get_band_freqs, get_bands, get_env, vocode,
-                             play_sound, window_edges, read_wav, rms)
+from expyfun.stimuli import vocode, play_sound, window_edges, read_wav, rms
 from expyfun import fetch_data_file
 
 data, fs = read_wav(fetch_data_file('audio/dream.wav'))
@@ -25,16 +24,7 @@ data_noise = data_noise * 0.01 / rms(data_noise)
 data_tone = vocode(data, fs, mode='tone')
 data_tone = data_tone * 0.01 / rms(data_tone)
 # poisson vocoder
-click_rate = 400  # poisson lambda (mean clicks / second)
-prob = click_rate / float(fs)
-carrier = np.random.choice([0., 1.], data.shape[-1], p=[1 - prob, prob])
-edges = get_band_freqs(fs)
-bands, filts = get_bands(data, fs, edges, zero_phase=True)
-envs, env_filts = zip(*[get_env(x, fs, zero_phase=True) for x in bands])
-carrs, carr_filts = get_bands(carrier, fs, edges, zero_phase=True)
-data_click = np.zeros_like(data)
-for carr, env in zip(carrs, envs):
-    data_click += carr * env
+data_click = vocode(data, fs, mode='poisson', rate=400)
 data_click = data_click * 0.01 / rms(data_click)
 
 # combine all three
