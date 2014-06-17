b6b0aa5d23fc17fc68fc2f08ff1c4dcecfe6e373
labsner <dpospisi@students.pitzer.edu>
2014-06-16 17:15:41 -0700

    pushing latest text fix changes

--- examples/keypress_demo.py
+++ examples/keypress_demo.py
@@ -28,9 +28,9 @@ with ExperimentController('KeypressDemo', screen_num=0,
 
     ###############
     # screen_prompt
-    pressed = ec.screen_prompt('press any key<br><br>screen_prompt('
+    pressed = ec.screen_prompt('press any key screen_prompt('
                                'max_wait={})'.format(wait_dur),
-                               max_wait=wait_dur, timestamp=True)
+                               max_wait=wait_dur, timestamp=True, )
     ec.write_data_line('screen_prompt', pressed)
     if pressed[0] is None:
         message = 'no keys pressed'
@@ -43,7 +43,7 @@ with ExperimentController('KeypressDemo', screen_num=0,
     ##################
     # wait_for_presses
     ec.screen_text('press some keys wait_for_presses(max_wait={})'
-                   ''.format(wait_dur))
+                   ''.format(wait_dur), )
     screenshot = ec.screenshot()
     ec.flip()
     pressed = ec.wait_for_presses(wait_dur)
@@ -51,15 +51,15 @@ with ExperimentController('KeypressDemo', screen_num=0,
     if not len(pressed):
         message = 'no keys pressed'
     else:
-        message = ['{} pressed after {} secs'
+        message = [' {} pressed after {} secs  \n'
                    ''.format(key, round(time, 4)) for key, time in pressed]
-        message = '<br>'.join(message)
-    ec.screen_prompt(message, msg_dur)
+        message = ''.join(message)
+    ec.screen_prompt(message, msg_dur,)
     ec.wait_secs(isi)
 
     ############################################
     # wait_for_presses, relative to master clock
-    ec.screen_text('press some keys wait_for_presses(max_wait={},<br>'
+    ec.screen_text('press some keys wait_for_presses(max_wait={}, '
                    'relative_to=0.0)'.format(wait_dur))
     ec.flip()
     pressed = ec.wait_for_presses(wait_dur, relative_to=0.0)
@@ -67,16 +67,16 @@ with ExperimentController('KeypressDemo', screen_num=0,
     if not len(pressed):
         message = 'no keys pressed'
     else:
-        message = ['{} pressed at {} secs'
+        message = ['{} pressed at {} secs  \n'
                    ''.format(key, round(time, 4)) for key, time in pressed]
-        message = '<br>'.join(message)
-    ec.screen_prompt(message, msg_dur)
+        message = ''.join(message)
+    ec.screen_prompt(message, msg_dur,)
     ec.wait_secs(isi)
 
     ##########################################
     # listen_presses / wait_secs / get_presses
-    ec.screen_text('press some keys<br><br>listen_presses()<br>wait_secs({0})'
-                   '<br>get_presses()'.format(wait_dur))
+    ec.screen_text('press some keys listen_presses() wait_secs({0})'
+                   ' get_presses()'.format(wait_dur))
     ec.flip()
     ec.listen_presses()
     ec.wait_secs(wait_dur)
@@ -85,16 +85,16 @@ with ExperimentController('KeypressDemo', screen_num=0,
     if not len(pressed):
         message = 'no keys pressed'
     else:
-        message = ['{} pressed after {} secs'
+        message = ['{} pressed after {} secs \n'
                    ''.format(key, round(time, 4)) for key, time in pressed]
-        message = '<br>'.join(message)
-    ec.screen_prompt(message, msg_dur)
+        message = ''.join(message)
+    ec.screen_prompt(message, msg_dur,)
     ec.wait_secs(isi)
 
     ####################################################################
     # listen_presses / wait_secs / get_presses, relative to master clock
-    ec.screen_text('press a few keys<br><br>listen_presses()'
-                   '<br>wait_secs({0})<br>get_presses(relative_to=0.0)'
+    ec.screen_text('press a few keys listen_presses()'
+                   ' wait_secs({0}) get_presses(relative_to=0.0)'
                    ''.format(wait_dur))
     ec.flip()
     ec.listen_presses()
@@ -104,10 +104,10 @@ with ExperimentController('KeypressDemo', screen_num=0,
     if not len(pressed):
         message = 'no keys pressed'
     else:
-        message = ['{} pressed at {} secs'
+        message = ['{} pressed at {} secs \n'
                    ''.format(key, round(time, 4)) for key, time in pressed]
-        message = '<br>'.join(message)
-    ec.screen_prompt(message, msg_dur)
+        message = ''.join(message)
+    ec.screen_prompt(message, msg_dur,)
     ec.wait_secs(isi)
 
     ###########################################
@@ -115,26 +115,26 @@ with ExperimentController('KeypressDemo', screen_num=0,
     disp_time = wait_dur
     countdown = ec.current_time + disp_time
     ec.call_on_next_flip(ec.listen_presses)
-    ec.screen_text('press some keys<br><br>listen_presses()'
-                   '<br>while loop {}<br>get_presses()'.format(disp_time))
+    ec.screen_text('press some keys listen_presses()'
+                   ' while loop {} get_presses()'.format(disp_time),)
     ec.flip()
     while ec.current_time < countdown:
         cur_time = round(countdown - ec.current_time, 1)
         if cur_time != disp_time:
             disp_time = cur_time
             # redraw text with updated disp_time
-            ec.screen_text('press some keys<br><br>listen_presses()<br>'
-                           'while loop {}<br>get_presses()'.format(disp_time))
+            ec.screen_text('press some keys listen_presses() '
+                           'while loop {} get_presses()'.format(disp_time))
             ec.flip()
     pressed = ec.get_presses()
     ec.write_data_line('listen / while / get_presses', pressed)
     if not len(pressed):
         message = 'no keys pressed'
     else:
-        message = ['{} pressed after {} secs'
+        message = ['{} pressed after {} secs \n'
                    ''.format(key, round(time, 4)) for key, time in pressed]
-        message = '<br>'.join(message)
-    ec.screen_prompt(message, msg_dur)
+        message = ''.join(message)
+    ec.screen_prompt(message, msg_dur,)
     ec.wait_secs(isi)
 
     #####################################################################
@@ -142,27 +142,27 @@ with ExperimentController('KeypressDemo', screen_num=0,
     disp_time = wait_dur
     countdown = ec.current_time + disp_time
     ec.call_on_next_flip(ec.listen_presses)
-    ec.screen_text('press some keys<br><br>listen_presses()<br>while loop '
-                   '{}<br>get_presses(relative_to=0.0)'.format(disp_time))
+    ec.screen_text('press some keys listen_presses() while loop '
+                   '{} get_presses(relative_to=0.0)'.format(disp_time))
     ec.flip()
     while ec.current_time < countdown:
         cur_time = round(countdown - ec.current_time, 1)
         if cur_time != disp_time:
             disp_time = cur_time
             # redraw text with updated disp_time
-            ec.screen_text('press some keys<br><br>listen_presses()<br>while '
-                           'loop {}<br>get_presses(relative_to=0.0)'
-                           ''.format(disp_time))
+            ec.screen_text('press some keys listen_presses() while '
+                           'loop {} get_presses(relative_to=0.0)'
+                           ''.format(disp_time),)
             ec.flip()
     pressed = ec.get_presses(relative_to=0.0)
     ec.write_data_line('listen / while / get_presses relative_to 0.0', pressed)
     if not len(pressed):
         message = 'no keys pressed'
     else:
-        message = ['{} pressed at {} secs'
+        message = ['{} pressed at {} secs \n'
                    ''.format(key, round(time, 4)) for key, time in pressed]
-        message = '<br>'.join(message)
-    ec.screen_prompt(message, msg_dur)
+        message = ''.join(message)
+    ec.screen_prompt(message, msg_dur,)
 
 import matplotlib.pyplot as plt
 plt.ion()
--- expyfun/tests/test_experiment_controller.py
+++ expyfun/tests/test_experiment_controller.py
@@ -334,11 +334,12 @@ def test_visual(ac=None):
         assert_raises(ValueError, line.set_coords, [0])
         line.set_coords([0, 1])
         ec.set_background_color('black')
-        text=visual.Text(ec, 'Hey', pos=[0, 0], color=[1,1,1], wrap=False)
+        text = visual.Text(ec, 'Hello Everybody!', pos=[0, 0], color=[1, 1, 1],
+                           wrap=False)
         text.draw()
-        text=visual.Text(ec, 'Hey', pos=[0, 0], color=[1,1,1], wrap=True)
+        text = visual.Text(ec, 'Thank you, come again.', pos=[0, 0],
+                           color=[1, 1, 1])
         text.draw()
-
 
 
 @interactive_test
