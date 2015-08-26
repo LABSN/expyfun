4ca7afb43f4bb45ec590a651c21aad8613e9d8cb
Ross Maddox <rkmaddox@uw.edu>
2015-08-25 17:00:56 -0700

    some fixes

--- expyfun/stimuli/__init__.py
+++ expyfun/stimuli/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2014, LABSN.
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 
+
 from ._hrtf import convolve_hrtf
 from ._mls import compute_mls_impulse_response, repeated_mls
 from ._stimuli import rms, play_sound, window_edges
@@ -9,6 +10,7 @@ from ._vocoder import vocode, get_band_freqs, get_bands, get_env, get_carriers
 from ._tracker import TrackerUD, TrackerBinom, TrackerDealer
 from .._tdt_controller import get_tdt_rates
 from ._texture import texture_ERB
+from ._crm import crm_sentence, crm_response_menu
 
 # for backward compat (not great to do this...)
 from ..io import read_wav, write_wav
