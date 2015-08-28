ed55f4bbf01e2891a42483891ca7bf647844ac98
Ross Maddox <rkmaddox@uw.edu>
2015-08-28 16:41:29 -0700

    update CIs

--- expyfun/stimuli/_filter.py
+++ expyfun/stimuli/_filter.py
@@ -12,52 +12,6 @@ from mne.filter import resample as _resample
 from .._parallel import parallel_func, _check_n_jobs
 
 
-def _fft_resample(x, W, new_len, npad, to_remove):
-    """Do FFT resampling with a filter function (possibly using CUDA)
-
-    Parameters
-    ----------
-    x : 1-d array
-        The array to resample.
-    W : 1-d array or gpuarray
-        The filtering function to apply.
-    new_len : int
-        The size of the output array (before removing padding).
-    npad : int
-        Amount of padding to apply before resampling.
-    to_remove : int
-        Number of samples to remove after resampling.
-    cuda_dict : dict
-        Dictionary constructed using setup_cuda_multiply_repeated().
-
-    Returns
-    -------
-    x : 1-d array
-        Filtered version of x.
-    """
-    # add some padding at beginning and end to make this work a little cleaner
-    x = _smart_pad(x, npad)
-    old_len = len(x)
-    N = int(min(new_len, old_len))
-    sl_1 = slice((N + 1) // 2)
-    y_fft = np.zeros(new_len, np.complex128)
-    x_fft = fft(x).ravel()
-    x_fft *= W
-    y_fft[sl_1] = x_fft[sl_1]
-    sl_2 = slice(-(N - 1) // 2, None)
-    y_fft[sl_2] = x_fft[sl_2]
-    y = np.real(ifft(y_fft, overwrite_x=True)).ravel()
-
-    # now let's trim it back to the correct size (if there was padding)
-    if to_remove > 0:
-        keep = np.ones((new_len), dtype='bool')
-        keep[:to_remove] = False
-        keep[-to_remove:] = False
-        y = np.compress(keep, y)
-
-    return y
-
-
 def _smart_pad(x, n_pad):
     """Pad vector x
     """
