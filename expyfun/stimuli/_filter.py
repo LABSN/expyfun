2feb8c6fb0531da116e95115c2bfc154d634cfcb
Ross Maddox <rkmaddox@uw.edu>
2015-08-31 11:00:33 -0700

    cleanup

--- expyfun/stimuli/_filter.py
+++ expyfun/stimuli/_filter.py
@@ -2,9 +2,16 @@
 """Stimulus resampling functions
 """
 
-from distutils.version import LooseVersion
+try:
+    from mne.filter import resample as _resample
+except ImportError:
+    _resample = None
 
-import mne
-if LooseVersion(mne.__version__) < LooseVersion('0.8'):
-    raise ImportError('mne-python too old')
-from mne.filter import resample  # NOQA
+
+def resample(*args, **kwargs):
+    _resample.__doc__
+    return _resample(*args, **kwargs)
+
+# Get the current docstring from mne, rather than a potentially stale one
+if _resample is not None:
+    resample.__doc__ = _resample.__doc__
