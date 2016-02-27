28c35374edab6ead02cbc73612b87333709f15ed
drammock <dan.mccloy@gmail.com>
2016-02-26 16:06:20 -0800

    update class name in init

--- expyfun/visual/__init__.py
+++ expyfun/visual/__init__.py
@@ -1,3 +1,3 @@
 from ._visual import (Text, Line, Triangle, Rectangle, Circle, RawImage,
-                      Diamond, ConcentricCircles, FixationDot, Movie,
+                      Diamond, ConcentricCircles, FixationDot, Video,
                       _convert_color, _Triangular)
