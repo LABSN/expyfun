fccea875ab231a00b67a3bdb96d07caf8bd7565a
rkmaddox <ross.maddox@rochester.edu>
2017-04-12 15:03:41 -0400

    fix wording

--- examples/stimuli/plot_tracker_staircase.py
+++ examples/stimuli/plot_tracker_staircase.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-===========================
-Do a couple adaptive tracks
-===========================
+==============================
+Do an adaptive track staircase
+==============================
 
 This shows how to make and use an adaptive track using AdaptiveTracker.
 
