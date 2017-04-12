cceeb0c07037f2856e9a8a939fff0a57613f67b0
Maddy Cappelloni <mcappel3@u.rochester.edu>
2017-04-12 15:57:41 -0400

    more oops

--- expyfun/stimuli/_hrtf.py
+++ expyfun/stimuli/_hrtf.py
@@ -74,7 +74,7 @@ def _get_hrtf(angle, source, fs, interp=False):
         brir = brir[idx[0]].copy()
     elif interp:
         idx = np.where(angles < read_angle)[0]
-        if max(idx) > :
+        if idx > max(angles):
             raise ValueError('angle "{0}" must be small than "{1}"'
                              ''.format(angle, max(angles)))
         # angles are
