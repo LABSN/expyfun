6b416406d76ce599138ddcdd93983dba2599c216
Maddy Cappelloni <mcappel3@u.rochester.edu>
2017-06-08 10:54:31 -0400

    add example

--- examples/stimuli/tracker_dealer_doublesided.py
+++ examples/stimuli/tracker_dealer_doublesided.py
@@ -5,10 +5,8 @@ Tracker Dealer Example
 ======================
 
 This file shows how to interleave multiple Tracker objects using
-:class:`expyfun.stimuli.TrackerDealer`.
-
-In this case, a modeled human subject generates two curves (one for each trial
-type: 1 & 2).
+:class:`expyfun.stimuli.TrackerDealer` to simultaneously approach a threshold
+from both above and below.
 
 @author: maddycapp27
 """
@@ -19,27 +17,28 @@ from expyfun.analyze import sigmoid
 import matplotlib.pyplot as plt
 
 # define parameters of modeled subject (using sigmoid probability)
-true_thresh = [30, 40]  # true thresholds for trial types 1 and 2
+true_thresh = 30  # true thresholds for trial types 1 and 2
 slope = 0.1
 chance = 0.5
 
 ##############################################################################
 # Defining Tracker Parameters
 # ---------------------------
-# In this example, the tracker parameters are exactly the same for each
-# instance of the up-down adaptive tracker. These are defined such that the
-# step sizes vary for both up v. down (the up step size is larger by a factor
-# of 3) and based on the number of reversals (the first element in each
-# list is the step size until the number of reversals dictacted by the second
-# element in change_criteria have occured (i.e. the up step size will be 9
-# until 5 reversals have occured, then the up step size will be 3.))
+# In this example, the tracker parameters are the same for each instance of
+# the up-down adaptive tracker except for the start value. Each start value in
+# the list will be given to a different tracker. The other parameters are 
+# defined such that the step sizes vary for both up v. down (the up step size
+# is larger by a factor of 3) and based on the number of reversals (the first
+# element in each list is the step size until the number of reversals dictated
+# by the second element in change_criteria have occured (i.e. the up step size
+# will be 9 until 5 reversals have occured, then the up step size will be 3.))
 up = 1
 down = 1
 step_size_up = [9, 3]
 step_size_down = [3, 1]
 stop_criterion = 30
 stop_rule = 'reversals'
-start_value = 45
+start_value = [15, 45]
 change_criteria = [0, 5]
 change_rule = 'reversals'
 x_min = 0
@@ -53,23 +52,24 @@ def callback(event_type, value=None, timestamp=None):
 
 # parameters for the tracker dealer
 max_lag = 2
-rng_dealer = np.random.RandomState(3)  # random seed for selecting trial type
+rng_dealer = np.random.RandomState(4)  # random seed for selecting trial type
 
 ##############################################################################
 # Initializing and Running Trackers
 # ---------------------------------
-# The two trackers in this example use all of the same parameters and then are
-# passed into the dealer. After the dealer is created, the type of each trial
-# (returned as an index of the array of individual trackers) and trial level
-# for that trial can be acquired.
+# The two trackers in this example use all of the same parameters except for
+# the start valueand then are passed into the dealer. After the dealer is
+# created, the type of trial with the start value above or below the true
+# threshold (returned as an index) and trial level for that trial can be
+# acquired.
 
-# initialize two tracker objects--one for each trial type
+# initialize two tracker objects--one for each start value
 tr_ud = [TrackerUD(callback, up, down, step_size_up, step_size_down,
-                   stop_criterion, stop_rule, start_value,
-                   change_criteria, change_rule, x_min, x_max) for i in [0, 1]]
+                   stop_criterion, stop_rule, sv, change_criteria, 
+                   change_rule, x_min, x_max) for sv in start_value]
 
 # initialize TrackerDealer object
-tr = TrackerDealer(tr_ud, max_lag, rng_dealer)
+tr = TrackerDealer(tr_UD, max_lag, rng_dealer)
 
 # Initialize human state
 rng_human = np.random.RandomState(1)  # random seed for modeled subject
@@ -78,7 +78,7 @@ while not tr.stopped:
     # Get information of which trial type is next and what the level is at
     # that time from TrackerDealer
     __, level = tr.get_trial()
-    tr_ud.respond(rng_human.rand() < sigmoid(level - true_thresh[ss],
+    tr_UD.respond(rng_human.rand() < sigmoid(level - true_thresh,
                                              lower=chance, slope=slope))
 
 ##############################################################################
@@ -94,6 +94,6 @@ for i in [0, 1]:
     lines[2].set_label('Estimated threshold')
 
     ax.legend(loc='best')
-    ax.set_title('Adaptive track of model human trial type {} (true threshold '
-                 'is {})'.format(i + 1, true_thresh[i]))
+    ax.set_title('Adaptive track with start value {} (true threshold '
+                 'is {})'.format(start_value[i], true_thresh))
     fig.tight_layout()
