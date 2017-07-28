adec923b3fb8ec47c4d04b6017d9147d88b2c426
Maddy Cappelloni <mcappel3@u.rochester.edu>
2017-07-28 08:57:48 -0400

    make tracker hit ceiling/floor for coveralls

--- expyfun/stimuli/tests/test_tracker.py
+++ expyfun/stimuli/tests/test_tracker.py
@@ -73,8 +73,8 @@ def test_tracker_ud():
     tr = TrackerUD(None, 3, 1, [1, 0.5], [1, 0.5], 10, np.inf, 1,
                    change_indices=[2])
     tr.respond(True)
-    tr = TrackerUD(None, 3, 1, [1, 0.5], [1, 0.5], np.inf, 10, 1,
-                   change_indices=[2], change_rule='trials')
+    tr = TrackerUD(None, 1, 1, [3, 1.5], [1, 0.5], np.inf, 10, 1,
+                   change_indices=[2], change_rule='trials', x_min=0, x_max=3)
     for t in np.arange(2):  # run long enough to encounter change_indices
         tr.respond(True)
         tr.respond(False)
