dbc625e8b5bf840a936ec94bae1cbfed1f66ff78
Maddy Cappelloni <mcappel3@u.rochester.edu>
2017-08-31 17:06:19 -0400

    aesthetic stuff

--- examples/experiments/progress_bar.py
+++ examples/experiments/progress_bar.py
@@ -17,7 +17,7 @@ n_trials = 6
 max_wait = 0.1 if building_doc else np.inf
 wait_dur = 0.1 if building_doc else 0.5
 
-with ExperimentController('name', version='dev', window_size=[500, 500],
+with ExperimentController('name', version='dev', window_size=[800, 600],
                           full_screen=False, session='foo',
                           participant='foo') as ec:
 
@@ -25,7 +25,7 @@ with ExperimentController('name', version='dev', window_size=[500, 500],
     pb = ProgressBar(ec, [0, -.1, 1.5, .1], units='norm')
 
     ec.screen_prompt('Press the number shown on the screen. Start by pressing'
-                     ' 1.', font_size=12, live_keys=[1], max_wait=max_wait)
+                     ' 1.', font_size=16, live_keys=[1], max_wait=max_wait)
 
     for n in np.arange(n_trials) + 1:
         # subject does some task
@@ -43,7 +43,7 @@ with ExperimentController('name', version='dev', window_size=[500, 500],
             # display the progress bar with some text
             ec.screen_text('You\'ve completed {} %. Press any key to proceed.'
                            ''.format(percent), [0, .1], wrap=False,
-                           font_size=12)
+                           font_size=16)
             pb.draw()
             if n == 4:
                 screenshot = ec.screenshot()
