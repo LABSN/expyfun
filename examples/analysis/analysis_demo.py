5627e18588537698295b27d02a9d648a90357b5a
Daniel McCloy <dan.mccloy@gmail.com>
2018-04-25 10:24:53 -0700

    fix demo

--- examples/analysis/analysis_demo.py
+++ examples/analysis/analysis_demo.py
@@ -5,7 +5,7 @@ Analysis demo
 =============
 
 This example simulates some 2AFC data and demonstrates the analysis
-functions ``dprime_2afc()`` and ``barplot()``.
+functions ``dprime()`` and ``barplot()``.
 """
 # Author: Dan McCloy <drmccloy@uw.edu>
 #
@@ -20,20 +20,27 @@ import expyfun.analyze as ea
 print(__doc__)
 
 # simulate some 2AFC data
-trials = 100
-c_prob = 0.9
-t_prob = 0.6
+targets = 50
+foils = 50
+a_prob = 0.9
+b_prob = 0.6
+f_prob = 0.2
 subjs = ['a', 'b', 'c', 'd', 'e']
-ctrl = np.random.binomial(trials, c_prob, len(subjs))
-test = np.random.binomial(trials, t_prob, len(subjs))
-ctrl_miss = trials - ctrl
-test_miss = trials - test
-data = pd.DataFrame(dict(ctrl_hit=ctrl, ctrl_miss=ctrl_miss,
-                         test_hit=test, test_miss=test_miss), index=subjs)
+a_hit = np.random.binomial(targets, a_prob, len(subjs))
+b_hit = np.random.binomial(targets, b_prob, len(subjs))
+a_fa = np.random.binomial(foils, f_prob, len(subjs))
+b_fa = np.random.binomial(foils, f_prob, len(subjs))
+a_miss = targets - a_hit
+b_miss = targets - b_hit
+a_cr = foils - a_fa
+b_cr = foils - b_fa
+data = pd.DataFrame(dict(a_hit=a_hit, a_miss=a_miss, a_fa=a_fa, a_cr=a_cr,
+                         b_hit=b_hit, b_miss=b_miss, b_fa=b_fa, b_cr=b_cr),
+                    index=subjs)
 # calculate dprimes
-ctrl_dprime = ea.dprime_2afc(data[['ctrl_hit', 'ctrl_miss']])
-test_dprime = ea.dprime_2afc(data[['test_hit', 'test_miss']])
-results = pd.DataFrame(dict(ctrl=ctrl_dprime, test=test_dprime))
+a_dprime = ea.dprime(data[['a_hit', 'a_miss', 'a_fa', 'a_cr']])
+b_dprime = ea.dprime(data[['b_hit', 'b_miss', 'b_fa', 'b_cr']])
+results = pd.DataFrame(dict(ctrl=a_dprime, test=b_dprime))
 # plot
 subplt, barplt = ea.barplot(results, axis=0, err_bars='sd', lines=True,
                             brackets=[(0, 1)], bracket_text=[r'$p < 10^{-9}$'])
@@ -46,16 +53,19 @@ conds = ['ctrl', 'test']
 diffs = ['easy', 'hard']
 colnames = ['-'.join([x, y]) for x, y in zip(conds * 2,
             np.tile(diffs, (2, 1)).T.ravel().tolist())]
-cprob = [0.8, 0.5]
-dprob = [0.9, 0.6]
-cblock = np.tile(np.atleast_2d(cprob).T, (2, len(subjs))).T
-dblock = np.tile(np.atleast_2d(np.repeat(dprob, 2)).T, len(subjs)).T
-probs = cblock * dblock
-rawscores = np.random.binomial(trials_per_cond, probs, (len(subjs),
-                                                        len(conds) *
-                                                        len(diffs)))
-hitmiss = np.c_[rawscores.ravel(), (trials_per_cond - rawscores).ravel()]
-dprimes = ea.dprime_2afc(hitmiss).reshape(rawscores.shape)
+cond_prob = [0.9, 0.8]
+diff_prob = [0.9, 0.7]
+cond_block = np.tile(np.atleast_2d(cond_prob).T, (2, len(subjs))).T
+diff_block = np.tile(np.atleast_2d(np.repeat(diff_prob, 2)).T, len(subjs)).T
+probs = cond_block * diff_block
+shape = (len(subjs), len(conds) * len(diffs))
+rawscores_targ = np.random.binomial(trials_per_cond, probs, shape)
+rawscores_foil = np.random.binomial(trials_per_cond, probs, shape)
+hmfc = np.c_[rawscores_targ.ravel(),
+             (trials_per_cond - rawscores_targ).ravel(),
+             (trials_per_cond - rawscores_foil).ravel(),
+             rawscores_foil.ravel()]
+dprimes = ea.dprime(hmfc).reshape(shape)
 results = pd.DataFrame(dprimes, index=subjs, columns=colnames)
 subplt, barplt = ea.barplot(results, axis=0, err_bars='sd', lines=True,
                             groups=[(0, 1), (2, 3)], group_names=diffs,
