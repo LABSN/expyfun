d162f7fc48d2375b3b48e606472d3a41f0508b03
Eric Larson <larson.eric.d@gmail.com>
2018-06-05 11:49:35 -0400

    FIX: avbin

---
+++ make/get_avbin.ps1
@@ -0,0 +1,37 @@
+$AVBIN_32_URL = "https://staff.washington.edu/larsoner/avbin.dll"
+
+function DownloadAVBin () {
+    $webclient = New-Object System.Net.WebClient
+    $basedir = $pwd.Path + "\"
+    Write-Host "Arch:" $Env:PYTHON_ARCH
+    if ($Env:PYTHON_ARCH -eq "32") {
+        $url = "https://staff.washington.edu/larsoner/avbin.dll"
+        $filepath = "C:\Windows\system32\avbin.dll"
+    } else {
+        $url = "https://staff.washington.edu/larsoner/avbin64.dll"
+        $filepath = "C:\Windows\system32\avbin64.dll"
+    }
+    Write-Host "Downloading" $url
+    $retry_attempts = 2
+    for($i=0; $i -lt $retry_attempts; $i++){
+        try {
+            $webclient.DownloadFile($url, $filepath)
+            break
+        }
+        Catch [Exception]{
+            Start-Sleep 1
+        }
+    }
+    if (Test-Path $filepath) {
+        Write-Host "File saved at" $filepath
+    } else {
+        # Retry once to get the error message if any at the last try
+        $webclient.DownloadFile($url, $filepath)
+    }
+}
+
+function main () {
+    DownloadAVBin
+}
+
+main
