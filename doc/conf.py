d97168133bfacab369286bc2387d596eed6d4f94
Eric Larson <larson.eric.d@gmail.com>
2021-04-27 11:26:47 -0400

    FIX: More

--- doc/conf.py
+++ doc/conf.py
@@ -261,7 +261,6 @@ sphinx_gallery_conf = {
     'doc_module': ('expyfun',),
     'examples_dirs': examples_dirs,
     'gallery_dirs': gallery_dirs,
-    'default_thumb_file': os.path.join('_static', 'favicon.ico'),
     'backreferences_dir': 'generated',
     'filename_pattern': r'/.*(?<!_)\.py$',  # anything that isn't *_.py
     'capture_repr': (),
