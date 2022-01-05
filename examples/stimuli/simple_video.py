c9c216f788d7f2797b6932fd8dd3b3cc0054dd97
Eric Larson <larson.eric.d@gmail.com>
2022-01-05 16:12:10 -0500

    FIX: Fix screenshot

--- examples/stimuli/simple_video.py
+++ examples/stimuli/simple_video.py
@@ -20,6 +20,7 @@ movie_path = fetch_data_file('video/example-video.mp4')
 ec_args = dict(exp_name='simple video example', window_size=(720, 480),
                full_screen=False, participant='foo', session='foo',
                version='dev', output_dir=None)
+screenshot = None
 
 with ExperimentController(**ec_args) as ec:
     ec.load_video(movie_path)
@@ -28,9 +29,10 @@ with ExperimentController(**ec_args) as ec:
     while not ec.video.finished:
         if ec.video.playing:
             fliptime = ec.flip()
+        if screenshot is None:
+            screenshot = ec.screenshot()
         if building_doc:
             break
-    screenshot = ec.screenshot()
     ec.delete_video()
     ec.flip()
     ec.screen_prompt('video over', max_wait=1.)
