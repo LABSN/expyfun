004e581d71d422049472f159ddc45bc46da4f0a5
tomstoll <tomstoll@med.umich.edu>
2025-09-16 15:57:06 -0400

    suppress_resamp to remove mne dependency in test

--- expyfun/tests/test_experiment_controller.py
+++ expyfun/tests/test_experiment_controller.py
@@ -811,6 +811,7 @@ def test_noise(hide_window):
         TypeError,
         ExperimentController,
         *std_args,
+        suppress_resamp=True,
         noise_dur='foo',
         **std_kwargs,
         )
@@ -818,11 +819,12 @@ def test_noise(hide_window):
         ValueError,
         ExperimentController,
         *std_args,
+        suppress_resamp=True,
         noise_dur=-5,
         **std_kwargs,
         )
     with ExperimentController(  # no error when noise_dur is a positive number
-            *std_args, noise_dur=10.2, **std_kwargs
+            *std_args, suppress_resamp=True, noise_dur=10.2, **std_kwargs
             ) as ec:
         ec.start_noise()
         ec.stop_noise()
@@ -830,20 +832,23 @@ def test_noise(hide_window):
     """Test that the noise_array can be set."""
     # check for ValueError if noise_array length is not a power of 2
     with ExperimentController(
-            *std_args, noise_array=noise_array[:1000], **std_kwargs
+            *std_args, suppress_resamp=True,
+            noise_array=noise_array[:1000], **std_kwargs
             ) as ec:
         with pytest.raises(ValueError):
             ec.start_noise()
         ec.stop_noise()
     # check that the RMS of noise_array must be close to 1
     with ExperimentController(
-            *std_args, noise_array=noise_array*0.01, **std_kwargs
+            *std_args, suppress_resamp=True,
+            noise_array=noise_array*0.01, **std_kwargs
             ) as ec:
         with pytest.raises(ValueError):
             ec.start_noise()
         ec.stop_noise()
     with ExperimentController(
-            *std_args, noise_array=noise_array*3, **std_kwargs
+            *std_args, suppress_resamp=True,
+            noise_array=noise_array*3, **std_kwargs
             ) as ec:
         with pytest.raises(ValueError):
             ec.start_noise()
@@ -851,19 +856,22 @@ def test_noise(hide_window):
 
     # no errors if len(noise_array) is a power of 2 and RMS==1
     with ExperimentController(
-        *std_args, noise_array=noise_array, **std_kwargs
+        *std_args, suppress_resamp=True,
+        noise_array=noise_array, **std_kwargs
     ) as ec:
         ec.start_noise()
         ec.stop_noise()
     # check that noise_array can have different RMS if check_noise_rms==False
     with ExperimentController(
-        *std_args, noise_array=noise_array*5, check_noise_rms=False,
+        *std_args, suppress_resamp=True,
+        noise_array=noise_array*5, check_noise_rms=False,
         **std_kwargs
     ) as ec:
         ec.start_noise()
         ec.stop_noise()
     with ExperimentController(
-            *std_args, noise_array=None, **std_kwargs
+            *std_args, suppress_resamp=True,
+            noise_array=None, **std_kwargs
             ) as ec:
         ec.start_noise()
         ec.stop_noise()
