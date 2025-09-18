6f2751fb5cc91a71c05f4a0a8707e8bf978a92d5
Eric Larson <larson.eric.d@gmail.com>
2025-09-18 17:23:01 -0400

    FIX: No need

--- expyfun/_experiment_controller.py
+++ expyfun/_experiment_controller.py
@@ -727,15 +727,13 @@ class ExperimentController:
             self.flip()
         return out
 
-    def set_background_color(self, color="black", *, draw=True):
+    def set_background_color(self, color="black"):
         """Set and draw a solid background color
 
         Parameters
         ----------
         color : matplotlib color
             The background color.
-        draw : bool
-            If True, draw the background color immediately.
 
         Notes
         -----
@@ -748,8 +746,7 @@ class ExperimentController:
         from pyglet import gl
 
         # we go a little over here to be safe from round-off errors
-        if draw:
-            Rectangle(self, pos=[0, 0, 2.1, 2.1], fill_color=color).draw()
+        Rectangle(self, pos=[0, 0, 2.1, 2.1], fill_color=color).draw()
         self._bgcolor = _convert_color(color)
         gl.glClearColor(*[c / 255.0 for c in self._bgcolor])
 
@@ -1089,7 +1086,7 @@ class ExperimentController:
         # enable blending
         gl.glEnable(gl.GL_BLEND)
         gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
-        self.set_background_color("black", draw=False)
+        self.set_background_color("black")
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
         v_ = False if os.getenv("_EXPYFUN_WIN_INVISIBLE") == "true" else True
         self.set_visible(v_)  # this is when we set fullscreen
